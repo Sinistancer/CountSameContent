eb4937dc27f1d70b89a9ad4b2dfe47e0da55e703
Mohammad Iqbal <mohammad.iqbal2329@gmail.com>
2020-02-22 02:36:38 +0700

    split code using function

--- CountSameContent.php
+++ CountSameContent.php
@@ -1,30 +1,58 @@
 <?php
+
+    function RecursiveToGetArrayContent($dir){
+
+        $scannedDirectory = array_diff(scandir($dir), array('..', '.'));
+        $arrayNextDir = array();
+        global $arrayContent;
+
+        foreach($scannedDirectory as $value){
+            if(is_dir($dir."\\".$value)){
+                array_push($arrayNextDir, $dir."\\".$value);
+            } else {
+                array_push($arrayContent, $dir."\\".$value);
+            }
+        }
+
+        foreach($arrayNextDir as $directory){
+            RecursiveToGetArrayContent($directory);
+        }
+
+        return $arrayContent;
+    }
+    
+    function CheckFileName($arrayContent){
+        
+
+        return $arrayGetContent;
+    }
+
+    function JoinAllContent($arrayContent){
+        $arrayGetContent = array();
+        foreach($arrayContent as $value){
+            $arrayGetContent[basename($value)] = file_get_contents($value);
+        }
+
+        return $arrayGetContent;
+    }
+    
+    function CountContent($arrayGetContent)
+    {
+        $setCount = array_count_values($arrayGetContent);
+        arsort($setCount);
+
+        $occurences = reset($setCount);
+        $most_frequent = key($setCount);
+
+        return $most_frequent.' '. $occurences;
+    }
+    
+    
     $dir    = 'C:\Users\Sinistancer\source\repos\DropsuiteTest';
-    $scanned_directory = array_diff(scandir($dir), array('..', '.'));
-    
-    //NANTI DIBKIN FUNCTION CEK JEROAN DIRECTORY
-    $arrayNextDir = array();
+
     $arrayContent = array();
-    foreach($scanned_directory as $value){
-       if(is_dir($dir."\\".$value)){
-        array_push($arrayNextDir, $dir."\\".$value);
-       } else {
-        array_push($arrayContent, $dir."\\".$value);
-       }
-    }
+    $getAllContent = RecursiveToGetArrayContent($dir);
+    $dataContent = JoinAllContent($getAllContent);
+    echo CountContent($dataContent);
 
-    //JOIN ALL CONTENT
-    $arrayGetContent = array('abcdef', 'abcdefghijk', 'abcd', 'abcd', 'abcdef', 'abcd');
-    foreach($arrayContent as $value){
-        array_push($arrayGetContent, file_get_contents($value));
-    }
     
-
-    //COUNT CONTENT
-    $setCount = array_count_values($arrayGetContent);
-    arsort($setCount);//Sort it from highest to lowest\
-
-    $occurences = reset($setCount); //get the first value (rewinds internal pointer )
-    $most_frequent = key($setCount); //get the key, as we are rewound it's the first key
-
-    echo $most_frequent.' '. $occurences;
