be9fc99af1efb8bf651eb91571a8fa0521f7055f
Mohammad Iqbal <mohammad.iqbal2329@gmail.com>
2020-02-22 03:38:52 +0700

    adding config.json, try adding to get big content and split function to class

--- CountSameContent.php
+++ CountSameContent.php
@@ -1,58 +1,135 @@
 <?php
-
-    function RecursiveToGetArrayContent($dir){
-
-        $scannedDirectory = array_diff(scandir($dir), array('..', '.'));
-        $arrayNextDir = array();
-        global $arrayContent;
-
-        foreach($scannedDirectory as $value){
-            if(is_dir($dir."\\".$value)){
-                array_push($arrayNextDir, $dir."\\".$value);
-            } else {
-                array_push($arrayContent, $dir."\\".$value);
+    class BigFile
+    {
+        protected $file;
+    
+        public function __construct($filename, $mode = "r")
+        {
+            if (!file_exists($filename)) {
+    
+                throw new Exception("File not found");
+    
+            }
+    
+            $this->file = new SplFileObject($filename, $mode);
+        }
+    
+        protected function iterateText()
+        {
+            $count = 0;
+    
+            while (!$this->file->eof()) {
+    
+                yield $this->file->fgets();
+    
+                $count++;
+            }
+            return $count;
+        }
+    
+        protected function iterateBinary($bytes)
+        {
+            $count = 0;
+    
+            while (!$this->file->eof()) {
+    
+                yield $this->file->fread($bytes);
+    
+                $count++;
             }
         }
-
-        foreach($arrayNextDir as $directory){
-            RecursiveToGetArrayContent($directory);
-        }
-
-        return $arrayContent;
-    }
     
-    function CheckFileName($arrayContent){
-        
-
-        return $arrayGetContent;
-    }
-
-    function JoinAllContent($arrayContent){
-        $arrayGetContent = array();
-        foreach($arrayContent as $value){
-            $arrayGetContent[basename($value)] = file_get_contents($value);
-        }
-
-        return $arrayGetContent;
-    }
+        public function iterate($type = "Text", $bytes = NULL)
+        {
+            if ($type == "Text") {
     
-    function CountContent($arrayGetContent)
+                return new NoRewindIterator($this->iterateText());
+    
+            } else {
+    
+                return new NoRewindIterator($this->iterateBinary($bytes));
+            }
+    
+        }
+    }
+     
+    class GetContent
     {
-        $setCount = array_count_values($arrayGetContent);
-        arsort($setCount);
+        public $arrayContent = null;
 
-        $occurences = reset($setCount);
-        $most_frequent = key($setCount);
+        public function __construct($arrayContent) {
+            $this->arrayContent = $arrayContent;
+            // some other code to add some stuff to foo
+        }
 
-        return $most_frequent.' '. $occurences;
+        function RecursiveToGetArrayContent($dir){
+            if (!file_exists($dir)){
+                echo "File or directory is not exist check your path and setting path in config.json";
+                return null;
+            }
+
+            $scannedDirectory = array_diff(scandir($dir), array('..', '.'));
+            $arrayNextDir = array();
+            global $arrayContent;
+
+            foreach($scannedDirectory as $value){
+                if(is_dir($dir."\\".$value)){
+                    array_push($arrayNextDir, $dir."\\".$value);
+                } else {
+                    array_push($arrayContent, $dir."\\".$value);
+                }
+            }
+
+            foreach($arrayNextDir as $directory){
+                $this->RecursiveToGetArrayContent($directory);
+            }
+
+            return $arrayContent;
+        }
+
+        function JoinAllContent($arrayContent){
+            $arrayGetContent = array();
+            foreach($arrayContent as $value){
+                $largefile = new BigFile($value);
+                $iterator = $largefile->iterate("Text"); // Text or Binary based on your file type
+                
+                $text = "";
+                foreach ($iterator as $line) {
+                    $text .= $line;
+                }
+
+                $arrayGetContent[basename($value)] = $text;
+            }
+
+            return $arrayGetContent;
+        }
+        
+        function CountContent($arrayGetContent)
+        {
+            $setCount = array_count_values($arrayGetContent);
+            arsort($setCount);
+
+            $occurences = reset($setCount);
+            $most_frequent = key($setCount);
+
+            return $most_frequent.' '. $occurences;
+        }
     }
-    
-    
-    $dir    = 'C:\Users\Sinistancer\source\repos\DropsuiteTest';
 
-    $arrayContent = array();
-    $getAllContent = RecursiveToGetArrayContent($dir);
-    $dataContent = JoinAllContent($getAllContent);
-    echo CountContent($dataContent);
+    if (file_exists("config.json")) {
+        $jsonStr = file_get_contents("config.json");
+        $config = json_decode($jsonStr);
 
-    
+        $dir = $config->path_directory;
+
+        $arrayContent = array();
+        $getContent = new GetContent($arrayContent);
+
+        $getAllContent = $getContent->RecursiveToGetArrayContent($dir);
+        if($getAllContent!=null){
+            $dataContent = $getContent->JoinAllContent($getAllContent);
+            echo $getContent->CountContent($dataContent);
+        }
+    } else {
+        echo "config.json must exists for DYNAMICALLY path";
+    }
